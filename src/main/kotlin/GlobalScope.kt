7768656de1341a38fce2a351345901977c263f4a
eduVieiraAF <edu_vieira@hotmail.com>
2022-06-08 03:10:57 -0300

    GlobalScope to be improved

---
+++ src/main/kotlin/GlobalScope.kt
@@ -0,0 +1,26 @@
+@file:Suppress("OPT_IN_IS_NOT_ENABLED")
+
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@OptIn(DelicateCoroutinesApi::class)
+fun main() { // main thread
+
+    println("Main program started → ${Thread.currentThread().name}")
+
+    GlobalScope.launch { // background coroutine that runs on a background thread
+        println("Dummy fake work started → ${Thread.currentThread().name}")
+
+        for (i in 0..4) {
+            delay(1000)
+            println("\t$i")
+        }
+
+        println("Dummy fake work ended → ${Thread.currentThread().name}")
+    }
+
+    Thread.sleep(6000) // waiting for coroutine to finish
+    println("Main program ended → ${Thread.currentThread().name}")
+}
