f67d28ed5b0ff857d6479093adf9d9bb3927fdb9
eduVieiraAF <edu_vieira@hotmail.com>
2022-06-08 02:27:30 -0300

    More studies and more trials

---
+++ src/main/kotlin/runBlocking.kt
@@ -0,0 +1,26 @@
+/* runBlocking is also a coroutine builder that bridges the non-coroutine world of a regular fun main() and the
+    code with coroutines inside of runBlocking { ... } curly braces. This is highlighted in an IDE by this:
+    CoroutineScope hint right after the runBlocking opening curly brace.*/
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main(): Unit = runBlocking {
+    println("Task started")
+    launch {
+        delay(2000)
+        println("World")
+        doSomething()
+    }
+
+    println("Hello, ")
+}
+
+suspend fun doSomething() {
+    for (i in 0..4) {
+        delay(1000)
+        println(i)
+    }
+    println("Now it's over")
+}
